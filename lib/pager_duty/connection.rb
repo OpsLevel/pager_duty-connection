15eb97b0b9b8d80549ab661cb1209dd6896300c4
Aric Walker <aricwalker@github.com>
2021-08-27 13:32:14 -0600

    Include case to handle Array response parsing

--- lib/pager_duty/connection.rb
+++ lib/pager_duty/connection.rb
@@ -130,6 +130,8 @@ module PagerDuty
           end
 
           body
+        when Array
+          body.map! { |element| parse(element) }
         else
           raise "Can't parse times of #{body.class}: #{body}"
         end
